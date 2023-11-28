c1f63a8d4c439fcd82e93905dee1a69e492a9a6d
Anshuman Dash <anshuman.smartters@gmail.com>
2023-11-28 16:48:19 +0530

    small changes

--- src/components/About_us/index.js
+++ src/components/About_us/index.js
@@ -3,7 +3,7 @@ import React from "react";
 function About() {
   return (
     <div>
-      <img src={`${process.env.NEXT_PUBLIC_BASE_PATH/images/BusinessImage1.svg}`} />
+      <img src="/images/BusinessImage1.svg" />
     </div>
   );
 }
