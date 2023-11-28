708aef83054326404d3690608c6d999747c50c70
Anshuman Dash <anshuman.smartters@gmail.com>
2023-11-28 15:39:39 +0530

    small changes

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 let envImageUnoptimize = process.env.NODE_ENV !== "production" ? false : true
 
 const nextConfig = {
+  
   output:  process.env.NODE_ENV !== "production" ? undefined: "export",
   images: {
     unoptimized: envImageUnoptimize,
