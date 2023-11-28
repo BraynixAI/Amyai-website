8a6f1cf9079597e118b897c68ebff2ec38a6f822
Anshuman Dash <anshuman.smartters@gmail.com>
2023-11-28 14:38:56 +0530

    small changes

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
+let envImageUnoptimize = process.env.NODE_ENV !== "production" ? false : true
+
 const nextConfig = {
-  reactStrictMode: true,
-  basePath:'/amyai'
+  output:  process.env.NODE_ENV !== "production" ? undefined: "export",
+  images: {
+    unoptimized: envImageUnoptimize,
+  },
 
 }
 
